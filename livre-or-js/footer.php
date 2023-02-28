9be1dc6a632b7786e86587e9c2b77315b4e960f9
gabriel.rigaud <gabriel.rigaud@laplateforme.io>
2023-02-28 12:46:06 +0100

    delet

--- livre-or-js/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <p class="copyright">Gabriel Compagnie / 2020 Copyright &copy;</p>
-    <div class="liens">
-        <a href="https://github.com/gabriel-rigaud" target="_blank"><img src="https://gabriel-rigaud.students-laplateforme.io/gif/github.gif" width="70" alt="Github"></a>
-        <a href="/" target="_blank"><img src="https://gabriel-rigaud.students-laplateforme.io/gif/cv.gif" width="70" alt="Cv"></a>
-    </div>
-</footer>
